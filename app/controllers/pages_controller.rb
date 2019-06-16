b0b59038331ac2d09f60546af9a080a7d7466379
vilson junior <vilsonrjunior@gmail.com>
2019-06-16 10:55:20 +0100

    added contact and careers to pages#controller and respective views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, expect: [:home, :contact, :careers]
 
   def home
   end
+
+  def contact
+  end
+
+  def careers
+  end
+
 end
