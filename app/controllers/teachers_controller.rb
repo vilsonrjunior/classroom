3f54b3ee8da20f7579a99f8e8b69ca79c3b1be9f
vilson junior <vilsonrjunior@gmail.com>
2019-06-17 12:25:46 +0100

    minor refactor on teachers controllers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,8 @@
 class TeachersController < ApplicationController
   skip_before_action :authenticate_user!
 
+  before_action :set_teacher, only: [:show, :edit, :update, :destroy]
+
   def create
     @teacher = Teacher.create(teacher_params)
     if @teacher.save
@@ -25,22 +27,18 @@ class TeachersController < ApplicationController
   end
 
   def show
-    @teacher = Teacher.find(params[:id])
   end
 
   def edit
-    @teacher = Teacher.find(params[:id])
   end
 
   def update
-    @teacher = Teacher.find(params[:id])
     @teacher.update(teacher_params)
     redirect_to teacher_path
   end
 
 
   def destroy
-    @teacher = Teacher.find(params[:id])
     @teacher.destroy
     redirect_to teachers_path
   end
@@ -49,5 +47,9 @@ class TeachersController < ApplicationController
 
   def teacher_params
     params.require(:teacher).permit(:first_name, :last_name, :language, :email)
+  end
+
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
   end
 end
